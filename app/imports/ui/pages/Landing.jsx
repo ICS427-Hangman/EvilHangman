d43dfa7bac4e4f0696008b5e991a5925f2892f26
malialiu <mliu0141@gmail.com>
2023-03-24 16:59:04 -1000

    Improve UI #17

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,17 +5,31 @@ import { Grid, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+      <Grid>
+        <Grid style={{ marginTop: '30px', marginBottom: '30px' }} id='landing-page' verticalAlign='middle' textAlign='center' container>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
+          <Grid.Row style={{ marginTop: '30px' }}>
+            <Grid.Column width={4}>
+              <Image src="/images/hangman.png"/>
+            </Grid.Column>
 
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
+            <Grid.Column width={8}>
+              <h1>Welcome to Hangman</h1>
+              <p>Now get to work and modify this app!</p>
+            </Grid.Column>
+          </Grid.Row>
 
+        </Grid>
+        <Grid style={{ marginTop: '40px' }} id='landing-page' verticalAlign='middle' container>
+          <Grid.Column>
+            <h1>Welcome to Hangman</h1>
+            <p style={{fontSize: '30px'}} >
+              1. <br/>
+              2. <br/>
+            </p>
+          </Grid.Column>
+
+        </Grid>
       </Grid>
     );
   }
