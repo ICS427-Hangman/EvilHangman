6c8aea6d0fb70351dab6054fdcc4adbb5209ed3f
malialiu <mliu0141@gmail.com>
2023-03-25 19:46:59 -1000

    Revert "Revert "Update Landing.jsx""
    This reverts commit 3107debac128dedd282c297d808247f114aead6b.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const linkStyle = { color: 'black', textDecoration: 'none' };
     return (
       <Grid>
         <Grid style={{ marginTop: '30px', marginBottom: '30px' }} id='landing-page' verticalAlign='middle' textAlign='center' container>
@@ -16,6 +18,10 @@ class Landing extends React.Component {
             <Grid.Column width={8}>
               <h1>Welcome to Hangman</h1>
               <p>Now get to work and modify this app!</p>
+              <button className="massive ui inverted red button">
+                <i className="heart icon"/>
+                <Link style={linkStyle} to="/hangman">Start</Link>
+              </button>
             </Grid.Column>
           </Grid.Row>
 
@@ -23,7 +29,7 @@ class Landing extends React.Component {
         <Grid style={{ marginTop: '40px' }} id='landing-page' verticalAlign='middle' container>
           <Grid.Column>
             <h1>Welcome to Hangman</h1>
-            <p style={{fontSize: '30px'}} >
+            <p style={ { fontSize: '30px' } } >
               1. <br/>
               2. <br/>
             </p>
