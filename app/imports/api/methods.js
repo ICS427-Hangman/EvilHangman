96163a7c334c3ffd6d15a9413e265609f4a030bc
zhixinli <98064045+zhixinliuh@users.noreply.github.com>
2023-04-15 15:49:51 -1000

    fix token method

--- app/imports/api/methods.js
+++ app/imports/api/methods.js
@@ -26,10 +26,13 @@ Meteor.methods({
       throw new Meteor.Error('invalid-email', 'The provided email address does not exist.');
     }
 
-    // Check if the user has security questions in their profile
-    const hasSecurityQuestions = user.profile && user.profile.securityQuestions && user.profile.securityQuestions.length > 0;
+    if (user.profile && user.profile.securityQuestions && user.profile.securityQuestions.length > 0) {
+      // Return a random security question from the user profile
+      const randomIndex = Math.floor(Math.random() * user.profile.securityQuestions.length);
+      return user.profile.securityQuestions[randomIndex];
+    }
 
-    return hasSecurityQuestions;
+    return null;
   },
   'users.createResetTokenWithSecurityQuestion'(email, securityQuestion, securityAnswer) {
     check(email, String);
@@ -39,24 +42,33 @@ Meteor.methods({
     const user = Meteor.users.findOne({ 'emails.address': email });
 
     if (!user) {
-      throw new Meteor.Error('invalid-email', 'Invalid email address');
+      throw new Meteor.Error('invalid-email', 'No account found with this email address');
     }
 
-    if (securityAnswer !== user.securityAnswer) {
-      throw new Meteor.Error('invalid-security-answer', 'Invalid security answer');
+    const correctSecurityQuestion = user.profile.securityQuestions.find(
+      (sq) => sq.question === securityQuestion && sq.answer === securityAnswer,
+    );
+
+    if (!correctSecurityQuestion) {
+      throw new Meteor.Error('invalid-answer', 'The security question answer is incorrect');
     }
 
-    const token = Random.secret();
+    const token = Random.hexString(20);
+    const tokenExpires = new Date();
+    tokenExpires.setHours(tokenExpires.getHours() + 48);
 
     Meteor.users.update(user._id, {
       $set: {
-        'services.password.reset': {
-          token,
-          tokenExpires: new Date(Date.now() + 3600000), // Token expires in 1 hour
-        },
+        'services.password.reset.token': token,
+        'services.password.reset.tokenExpires': tokenExpires,
       },
     });
 
+    // Remove this line to prevent sending emails
+    // this.unblock();
+    // Email.sendResetPasswordLink(user, token);
+
+    // Return the token after creating it
     return token;
   },
 
